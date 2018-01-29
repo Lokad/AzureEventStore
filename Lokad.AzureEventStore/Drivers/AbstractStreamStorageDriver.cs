d0b6c37656c28292c0426b4f8a0f94d215ad4642
Alexis Nikichine <alexis.nikichine@lokad.com>
2018-01-29 13:14:21 +0100

    reimplemented MemoryStorageDriver
    FileStorageDriver and MemoryStorageDriver now share all their code;
    one uses a FileStream as underlying storage whereas the other one
    uses a resizeable MemoryStream.
    
    We're gonna need this to implement tests of the "DiscardUpTo" which
    will test its behaviour in the face of "discards" that cause it to
    fetch more blocks from its storage driver

---
+++ Lokad.AzureEventStore/Drivers/AbstractStreamStorageDriver.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Lokad.AzureEventStore.Drivers
+{
+    /// <summary> A simple, inefficient, persistent stream-based storage driver. </summary>
+    /// <remarks> Intended for use during unit testing, or local development. </remarks>
+    internal abstract class AbstractStreamStorageDriver : IStorageDriver, IDisposable
+    {
+        private readonly Stream _file;
+
+        protected AbstractStreamStorageDriver(Stream stream)
+        {
+            _file = stream;
+        }
+
+        /// <see cref="GetPositionAsync"/>
+        public long GetPosition() => _file.Length;
+
+        public Task<long> GetPositionAsync(CancellationToken cancel = new CancellationToken()) =>
+            Task.FromResult(GetPosition());
+
+        public Task<DriverWriteResult> WriteAsync(long position, IEnumerable<RawEvent> events,
+            CancellationToken cancel = new CancellationToken())
+            => Task.FromResult(Write(position, events));
+
+        internal DriverWriteResult Write(long position, IEnumerable<RawEvent> events)
+        {
+            var written = false;
+
+            if (_file.Length == position)
+            {
+                written = true;
+                _file.Seek(0, SeekOrigin.End);
+
+                using (var w = new BinaryWriter(_file, Encoding.UTF8, true))
+                foreach (var e in events)
+                    EventFormat.Write(w, e);
+            }
+
+            return new DriverWriteResult(_file.Length, written);
+        }
+
+        public Task<DriverReadResult> ReadAsync(long position, long maxBytes,
+            CancellationToken cancel = new CancellationToken())
+            => Task.FromResult(Read(position, maxBytes));
+
+        internal DriverReadResult Read(long position, long maxBytes)
+        {
+            var events = new List<RawEvent>();
+
+            if (position >= _file.Length)
+                return new DriverReadResult(_file.Length, events);
+
+            _file.Seek(position, SeekOrigin.Begin);
+
+            var readBytes = 0L;
+
+            using (var r = new BinaryReader(_file, Encoding.UTF8, true))
+            {
+                var availableBytes = _file.Length - position;
+                while (readBytes < availableBytes)
+                {
+                    var evt = EventFormat.Read(r);
+                    var newReadBytes = _file.Position - position;
+
+                    if (events.Count == 0 || newReadBytes < maxBytes)
+                    {
+                        events.Add(evt);
+                        readBytes = newReadBytes;
+                    }
+
+                    if (newReadBytes >= maxBytes)
+                        break;
+                }
+            }
+
+            return new DriverReadResult(position + readBytes, events);
+        }
+        
+        public async Task<uint> GetLastKeyAsync(CancellationToken cancel = new CancellationToken()) => 
+            await EventFormat.GetLastSequenceAsync(_file, cancel);
+
+        public Task<long> SeekAsync(uint key, long position = 0, CancellationToken cancel = new CancellationToken()) => 
+            Task.FromResult(position);
+
+        public void Dispose()
+        {
+            _file.Dispose();
+        }
+    }
+}
--- Lokad.AzureEventStore/Drivers/MemoryStorageDriver.cs
+++ Lokad.AzureEventStore/Drivers/MemoryStorageDriver.cs
@@ -1,104 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Threading;
-using System.Threading.Tasks;
+﻿using System.IO;
 
 namespace Lokad.AzureEventStore.Drivers
 {
-    /// <summary>
-    /// A simple, inefficient, non-persistent <see cref="IStorageDriver"/>
-    /// that stores events in memory.
-    /// </summary>
-    /// <remarks>
-    /// The position is the event index in the sequence.
-    /// </remarks>
-    internal sealed class MemoryStorageDriver : IStorageDriver
+    /// <summary> A simple, inefficient, persistent file-based storage driver. </summary>
+    /// <remarks> Intended for use during local development. </remarks>
+    internal sealed class MemoryStorageDriver : AbstractStreamStorageDriver
     {
-        private readonly List<RawEvent> _events = new List<RawEvent>();
-
-        private long Position => _events.Count;
-
-        public Task<long> GetPositionAsync(CancellationToken cancel = new CancellationToken())
-        {
-            lock (this)
-                return Task.FromResult(Position);
-        }
-
-        internal DriverWriteResult Write(long position, IEnumerable<RawEvent> events)
-        {
-            if (position != _events.Count) return new DriverWriteResult(_events.Count, false);
-            
-            foreach (var e in events)
-            {
-                // We are not allowed to keep a reference to the original byte arrays,
-                // so copy them.
-                var b = new byte[e.Contents.Length];
-                Array.Copy(e.Contents, 0, b, 0, b.Length);
-                _events.Add(new RawEvent(e.Sequence, b));
-            }
-
-            return new DriverWriteResult(_events.Count, true);
-        }
-
-        public Task<DriverWriteResult> WriteAsync(long position, IEnumerable<RawEvent> events, CancellationToken cancel = new CancellationToken())
-        {
-            lock (this)
-                return Task.FromResult(Write(position, events));            
-        }
-
-        internal DriverReadResult Read(long position, long maxBytes)
-        {            
-            var list = new List<RawEvent>();
-            var copied = 0;
-            for (var i = (int) position; i < _events.Count; ++i)
-            {
-                var e = _events[i];                
-                copied += e.Contents.Length;
-
-                if (list.Count > 0 && copied > maxBytes) break;
-
-                // We do not want to hand out a reference to our mutable contents, 
-                // so copy them.
-                var b = new byte[e.Contents.Length];
-                Array.Copy(e.Contents, 0, b, 0, b.Length);
-
-                list.Add(new RawEvent(e.Sequence, b));
-            }
-
-            return new DriverReadResult(position + list.Count, list);
-        }
-
-        public Task<DriverReadResult> ReadAsync(long position, long maxBytes, CancellationToken cancel = new CancellationToken())
-        {
-            lock (this)
-                return Task.FromResult(Read(position, maxBytes));            
-        }
-
-        private uint LastKey => _events.Count == 0 ? 0U : _events[_events.Count - 1].Sequence;
-
-        public Task<uint> GetLastKeyAsync(CancellationToken cancel = new CancellationToken())
-        {
-            lock (this)
-                return Task.FromResult(LastKey);            
-        }
-
-        private long Seek(uint key, long position)
-        {
-            long a = position, b = _events.Count;
-            while (b - a > 1)
-            {
-                var m = (int)(a + b) / 2;
-                if (_events[m].Sequence > key) b = m;
-                else a = m;
-            }
-
-            return a;
-        }
-
-        public Task<long> SeekAsync(uint key, long position = 0, CancellationToken cancel = new CancellationToken())
-        {
-            lock (this)
-                return Task.FromResult(Seek(key, position));
-        }
+        internal MemoryStorageDriver() 
+            : base(new MemoryStream())
+        {}
     }
 }
